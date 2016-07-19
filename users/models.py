d0fe1aca56f4a3fca28aad461fcdf3e3ec6b4081
andres@boalt <andresgleztelo@gmail.com>
2016-07-19 18:25:55 -0400

    Css ouches

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class ChatUser(models.Model):
     """ A ChatUser corresponds to an individual user in our app. It can be associated
         with multiple conversations and chats. It has a one to one mapping to the corresponding
-        auth user """
+        auth user."""
     user = models.OneToOneField(User, related_name='chat_user', on_delete=models.CASCADE)
     identifier = models.CharField(max_length=10, blank=False, null=False, unique=True, db_index=True)
     is_active = models.BooleanField(default=True)
